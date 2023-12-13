246d540c28eb5cd8aaa5b21814cad25e1854c529
lamnphuc <2251012082lam@ou.edu.vn>
2023-12-13 13:52:11 +0700

    Update 13.12
    Some interest thing.

---
+++ main4.js
@@ -0,0 +1,25 @@
+var countDownDate = new Date("Dec 20, 2023 00:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
+
+var secondss = 1000;
+function displaysecondss() {
+  secondss -= 1;
+  document.getElementById("secondsdisplay").innerText =
+    "Trở về Trang Chủ sau " + secondss + " giây.";
+}
+setInterval(displaysecondss, 1000);
+function backtohome() {
+  window.location = "index.html";
+}
+setTimeout("backtohome()", 1000000);
